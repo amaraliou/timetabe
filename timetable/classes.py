e324df4f3f8df0876c69c58cbc13b78af88fa3ef
amaraliou <amarraliou97@gmail.com>
2018-09-06 17:21:31 +0100

    Setting up classes to handle the timetable ics file

---
+++ timetable/classes.py
@@ -0,0 +1,34 @@
+from icalendar import Calendar, Event
+from datetime import datetime, timedelta, tzinfo
+import dateutil
+import json
+import pytz
+import re
+
+LONDON = pytz.timezone('Europe/London')
+DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
+EMPTYTABLE = [[''] + DAYS] + [[str((datetime(2018, 9, 10, 9, 0, 0, 0, tzinfo = None) + timedelta(hours = i)).time())] + ['' for i in range(5)] for i in range(9)]
+DAYSTOINT = dict(zip(DAYS, [1,2,3,4,5]))
+INTODAYS = dict(zip([0,1,2,3,4], DAYS))
+LONGTOSHORT = {'Algs & Data Structures 2': 'ADS2',
+               'Java Programming 2': 'JP2',
+               'Web App Development 2': 'WAD2',
+               'Alg Foundations 2': 'AF2'}
+HOURSTOINT = {str((datetime(2018, 9, 10, 9, 0, 0, 0, tzinfo = None) + timedelta(hours = i)).time()): i + 1 for i in range(9)}
+
+class iCal:
+
+    def __init__(self):
+        pass
+
+
+class Timetable:
+
+    def __init__(self):
+        pass
+
+
+class Slot:
+
+    def __init__(self):
+        pass
